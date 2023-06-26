d37f835de5205d7e4292973a2b735fac8df41376
vadim12121212 <123749673+vadim12121212@users.noreply.github.com>
2023-06-26 11:04:59 +0300

    11

--- hw1_1.java
+++ hw1_1.java
@@ -1,18 +1,1 @@
-import java.util.Scanner;
 
-public class hw1_1 {
-    public static void main(String[] args) {
-        System.out.printf("Введите число n: ");
-        Scanner num = new Scanner(System.in);
-        int n = num.nextInt();
-        int sum = 0;
-        int mult = 1;
-        for (int i = 1; i <= n; i++) {
-            sum += i;
-            mult *= i;
-        }
-        System.out.printf("Сумма чисел от 1 до %d: %d%n", n, sum);
-        System.out.printf("Произведение чисел от 1 до %d: %d%n", n, mult);
-        num.close();
-    }
-}
