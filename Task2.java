575eed44e371504109cf143e34d79995e2e41838
*vadim12121212* <imailcom8@gmail.com>
2023-06-26 11:00:32 +0300

    Задача 3

---
+++ Task2.java
@@ -0,0 +1,19 @@
+// 2. Вывести все простые числа от 1 до 1000. 
+public class Task2 {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 1001; i++) {
+            // поставили флаг
+            boolean isPrime = true;
+            for (int j = 2; j <= Math.floor(Math.sqrt(i)); j++) {
+                // Если j - делитель, флаг = false
+                if ((i % j) == 0) {
+                    isPrime = false;
+                    break;
+                }
+            }
+            if (isPrime) {
+                System.out.print(i + " "); // если делителей нет, флаг остался True, число на печать
+            }
+        }
+    }
+}
